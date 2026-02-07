706e9a029139ad850ed43a7c05732d7af6ce779b
Hitansh <hitanshgandhi123@gmail.com>
2026-02-07 15:48:27 +0530

    Fixed Bugs in pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -22,9 +22,10 @@ if Path(file_path).is_file():
     def create_chapter(m, pg):
         doc_struct["chapters"].append({
             "chapter_id" : m.group(1),
-            "chapter_title" : m.group(2),
+            "chapter_title" : m.group(2).strip(),
             "page_start" : pg,
             "page_end" : None,
+            "chapter_text" : "",
             "sections" : []
         })
         return doc_struct["chapters"][-1]
@@ -42,40 +43,58 @@ if Path(file_path).is_file():
     def create_section(m, pg, curr_chap):
         curr_chap["sections"].append({
             "section_id" : m.group(1),
-            "section_title" : m.group(2),
+            "section_title" : m.group(2).strip(),
             "page_start" : pg,
             "page_end" : None,
             "raw_text" : ""
         })
-        return curr_chap[-1]
+        return curr_chap["sections"][-1]
     
     def close_curr_section(pg, curr_sec):
         curr_sec["page_end"] = pg
         return
     
     for page in pdf.pages:
-        for line in page.extract_text().splitlines():
-            m = is_chapter_name(line)
-            if m:
-                if current_chapter is not None:
-                    if line == page.extract_text().splitlines()[0]:
-                        close_curr_chap(page.page_number - 1, current_chapter)
-                    else:
-                        close_curr_chap(page.page_number, current_chapter)
-                current_chapter = create_chapter(m, page.page_number)
-                continue
-            m = is_section_name(line)
-            if m:
+        text = page.extract_text()
+        if text is None:
+            continue
+        lines = text.splitlines()
+        for idx, line in enumerate(lines):
+                line = line.strip()
+                m = is_chapter_name(line)
+                if m:
+                    if current_chapter is not None:
+                        if idx == 0:
+                            if current_section is not None:
+                                close_curr_section(page.page_number - 1, current_section)
+                                current_section = None
+                            close_curr_chap(page.page_number - 1, current_chapter)
+                            
+                        else:
+                            if current_section is not None:
+                                close_curr_section(page.page_number, current_section)
+                                current_section = None
+                            close_curr_chap(page.page_number, current_chapter)
+                            
+                    current_chapter = create_chapter(m, page.page_number)
+                    continue
+                m = is_section_name(line)
+                if m:
+                    if current_section is not None:
+                        if idx == 0:
+                            close_curr_section(page.page_number - 1, current_section)
+                        else:
+                            close_curr_section(page.page_number, current_section)
+                    current_section = create_section(m, page.page_number, current_chapter)
+                    continue
+                if current_chapter is not None and current_section is None:
+                    current_chapter["chapter_text"] += line + "\n"
                 if current_section is not None:
-                    if line == page.extract_text().splitlines()[0]:
-                        close_curr_section(page.page_number - 1, current_section)
-                    else:
-                        close_curr_section(page.page_number, current_section)
-                current_section = create_section(m, page.page_number, current_chapter)
-                continue
-            current_section["raw_text"] += line
-    close_curr_section(pdf.pages[-1].page_number, current_section)   
-    close_curr_chap(pdf.pages[-1].page_number, current_chapter)             
+                    current_section["raw_text"] += line + "\n"
+    if current_chapter is not None:
+        if current_section is not None:
+            close_curr_section(pdf.pages[-1].page_number, current_section)   
+        close_curr_chap(pdf.pages[-1].page_number, current_chapter)             
 
 else :
     print("Incorrect File Path.\nEnter a valid File Path.")
